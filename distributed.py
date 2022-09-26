b0ca280e3bb8514b29add0b1f2fdfd5d11f2748d
Jarem95 <48213567+Jarem95@users.noreply.github.com>
2022-09-26 16:53:32 +0200

    Update distributed.py

--- distributed.py
+++ distributed.py
@@ -9,6 +9,7 @@ from single_agent_planner import compute_heuristics, a_star, get_sum_of_cost
 from aircraft import AircraftDistributed
 from cbs import detect_collision, detect_collisions
 
+
 class DistributedPlanningSolver(object):
     """A distributed planner"""
 
@@ -24,29 +25,29 @@ class DistributedPlanningSolver(object):
         self.num_of_agents = len(goals)
         self.heuristics = []
         # T.B.D.
-        
+
     def find_solution(self):
         """
         Finds paths for all agents from start to goal locations. 
-        
+
         Returns:
             result (list): with a path [(s,t), .....] for each agent.
         """
-        # Initialize constants       
+        # Initialize constants
         start_time = timer.time()
         result = []
         self.CPU_time = timer.time() - start_time
-        
-        
+
         # Create agent objects with AircraftDistributed class
         for i in range(self.num_of_agents):
             newAgent = AircraftDistributed(self.my_map, self.starts[i], self.goals[i], self.heuristics[i], i)
-        
-        
+
         # Print final output
         print("\n Found a solution! \n")
         print("CPU time (s):    {:.2f}".format(self.CPU_time))
-        print("Sum of costs:    {}".format(get_sum_of_cost(result)))  # Hint: think about how cost is defined in your implementation
+        # Hint: think about how cost is defined in your implementation
+        print("Sum of costs:    {}".format(get_sum_of_cost(result)))
         print(result)
-        
-        return result  # Hint: this should be the final result of the distributed planning (visualization is done after planning)
+
+        # Hint: this should be the final result of the distributed planning (visualization is done after planning)
+        return result
